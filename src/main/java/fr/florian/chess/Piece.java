43e97ab22a5cf5b2ec9d036ef3a00e55e5346c77
Florian <floflomontage@gmail.com>
2022-10-12 23:50:31 +0200

    refacto

--- src/main/java/fr/florian/chess/Piece.java
+++ src/main/java/fr/florian/chess/Piece.java
@@ -1,25 +1,17 @@
-package fr.florian;
+package fr.florian.chess;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 
 public abstract class Piece {
     
     private Color couleur;
-    private ImageView image;
 
     public Piece(Color couleur) {
         this.couleur = couleur;
-        this.image = new ImageView(new Image("file:src/main/resources/images/" + this.toString() + "_100px.png"));
     }
 
     public Color getCouleur() {
         return couleur;
-    }
-
-    public ImageView getImage() {
-        return image;
     }
 
     public abstract String toString();
