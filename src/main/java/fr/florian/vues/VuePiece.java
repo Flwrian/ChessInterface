0b4af7b9e1fdc98d3fa717063edf29854fcb5618
Florian <floflomontage@gmail.com>
2022-10-13 02:19:57 +0200

    New resources

--- src/main/java/fr/florian/vues/VuePiece.java
+++ src/main/java/fr/florian/vues/VuePiece.java
@@ -1,12 +1,19 @@
 package fr.florian.vues;
 
-import fr.florian.IPiece;
+import fr.florian.chess.Piece;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class VuePiece extends ImageView {
 
-    public VuePiece(IPiece piece) {
-        super(new Image("file:src/main/resources/images/" + piece.getNom() + "_100px.png"));
+    private Piece piece;
+
+    public VuePiece(Piece piece) {
+        super(new Image(piece.toString() + "_" + piece.getCouleurString() + ".png"));
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
     }
 }
